31f8433c9457a1fd4db7747bd901de30c4dede14
GuiAquinoBR <59674194+GuiAquinoBR@users.noreply.github.com>
2021-08-02 19:06:31 -0300

    Create selecionar_contato_limit.py

---
+++ selecionar_contato_limit.py
@@ -0,0 +1,16 @@
+from pymysql import Error
+from db import nova_conexao
+
+sql = 'SELECT * FROM contatos LIMIT %s OFFSET %s'
+args = (5, 3)
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute(sql, args)
+        contatos = cursor.fetchall()
+    except Error as e:
+        print(f'Erro ocorrido: {e}')
+    else:
+        for contato in contatos:
+            print(f'{contato[2]:3d} - {contato[0]:10s} Telefone: {contato[1]}')

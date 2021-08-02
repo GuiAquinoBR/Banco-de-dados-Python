929dda78dd94b6029328349468a568861642d3b2
GuiAquinoBR <59674194+GuiAquinoBR@users.noreply.github.com>
2021-08-02 18:21:12 -0300

    Create incluir_varios_contatos.py
    Filling in the contatos table

---
+++ incluir_varios_contatos.py
@@ -0,0 +1,25 @@
+from pymysql import Error
+from db import nova_conexao
+
+sql = 'INSERT INTO contatos (nome, tel) VALUES (%s, %s)'
+dados = (
+    ('Guilherme',   '98765-1234'),
+    ('Inês',        '98765-9876'),
+    ('Nilton',      '98765-5432'),
+    ('Daiane',      '98765-1234'),
+    ('Leidiane',    '98765-5678'),
+    ('Washington',  '98765-9000'),
+    ('João',       '98754-9195'),
+    ('Maria',     '94132-4321'),
+    ('Cláudia',      '98765-5867'),
+)
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.executemany(sql, dados)
+        conexao.commit()
+    except Error as e:
+        print(f'Erro ocorrido: {e}')
+    else:
+        print(f'Foram incluídos {cursor.rowcount} registros!')
